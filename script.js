a6a80be4c3631b264dcc7db1bdc2e4d116498dcd
Ekd <191290150+Ekd-learning@users.noreply.github.com>
2024-12-14 18:22:09 +0300

    Player now can make a choice using a UI. Added empty div container to html file as well

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const rock_btn = document.createElement("button");
+rock_btn.setAttribute("id", "rock");
+rock_btn.textContent = "Rock";
+const paper_btn = document.createElement("button");
+paper_btn.setAttribute("id", "paper");
+paper_btn.textContent = "Paper";
+const scissors_btn = document.createElement("button");
+scissors_btn.setAttribute("id", "scissors");
+scissors_btn.textContent = "Scissors";
+document.body.appendChild(rock_btn, paper_btn, scissors_btn);
+document.body.appendChild(paper_btn);
+document.body.appendChild(scissors_btn);
+const result_container = document.createElement("div");
+result_container.setAttribute("id", "result");
+document.body.appendChild(document.createElement("br"));
+document.body.appendChild(result_container);
+
+//rock_btn.addEventListener("onClick", game());
+
 let humanScore = 0;
 let computerScore = 0;
 const getComputerChoice = function () {
@@ -39,7 +58,6 @@ const validateNumber = function (num) {
 const compare = function (human, comp) {
   if (human === comp) {
     // tie
-    console.log(`Tie, You and computer both chose ${comp}`);
     return 0;
   } else {
     if (human === "rock") {
@@ -74,11 +92,12 @@ const compare = function (human, comp) {
   }
 };
 
-const playRound = function () {
-  const human = getHumanChoice();
+const playRound = function (humanInput) {
+  const human = humanInput; //getHumanChoice();
   const comp = getComputerChoice();
   console.log(human, comp);
   const result = compare(human, comp);
+  console.log("compare result: ", result);
   if (result > 0) {
     humanScore++;
   } else if (result < 0) {
@@ -86,6 +105,26 @@ const playRound = function () {
   }
   console.log(
     `Current score is ${humanScore} : ${computerScore}\n(You : Computer)`
+  );
+  let color;
+  let res_message;
+  if (result < 0) {
+    color = "red";
+    res_message = "Computer beat you! What a loser!";
+  }
+  if (result > 0) {
+    color = "green";
+    res_message = "You beat the computer! Congratulations!";
+  }
+  if (result === 0) {
+    color = "white";
+    res_message = "Tie! None of you is a winner.";
+  }
+  displayResults(
+    res_message +
+      "\n" +
+      `Current score is ${humanScore} : ${computerScore}\n(You : Computer)`,
+    color
   );
 };
 
@@ -101,4 +140,15 @@ const game = function (roundCount) {
     console.log(`Tie! None of you is a winner.`);
   }
 };
-game(5);
+//game(5);
+
+const displayResults = function (text, color) {
+  result_container.style.backgroundColor = "black";
+  result_container.style.color = color;
+  result_container.textContent = "";
+  result_container.textContent = text;
+};
+
+rock_btn.addEventListener("click", () => playRound(rock_btn.id));
+paper_btn.addEventListener("click", () => playRound(paper_btn.id));
+scissors_btn.addEventListener("click", () => playRound(scissors_btn.id));
